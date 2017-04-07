a42ac2c6b66bf54dc7ffcd7593f91311584aa209
Francesca Giordano <fg6@sanger.ac.uk>
2017-04-07 14:44:39 +0100

    initial commit

--- settings.sh
+++ settings.sh
@@ -5,34 +5,38 @@ fafile=FAFILE
 # fastq file with long reads
 fqfile=FQFILE
 
-
-#bin directory: where the executables are
-bindir=$MYSMISDIR/smissv-bin
 # working directory
 odir=ODIR
 
+#bin directory: where the executables are
+bindir=$MYSMISDIR/smissv-bin
+
 #which samtools
 mysamtools=`which samtools`
-# which aligner
-aligner=bwa
+
+# running mode
+debug=0
 
 
+
+########### FAKE-MATES PARAMETERS: ###########
 # min length of long reads 
 minlen=3000
-# scaffold settings
-min_edge=5.0
-num_sigma=`awk "BEGIN {print ($min_edge)/2.5}"`
-# length of fake mate pairs
+# length of fake mates
 fakelen=2000
 # distance between one fake mate pairs and the next
 step=200
 
-### Alignment settings
+
+########### ALIGNMENT PARAMETERS: ###########
+
+# which aligner to use (bwa, smalt)
+aligner=bwa
+
 # bwa
 mybwa=`which bwa`
 # smalt 
 mysmalt=`which smalt`
-
 # max threads:  
 nodes=25
 #  minimum smith-waterman alignment score to report a hit 
@@ -45,15 +49,9 @@ gapopen=-1
 gapext=-1
 
 
-# running mode
-debug=0
 
-# Scaffolding settings
-# Settings for selecting indels
-distance_length=2500
-# minimum alignment score 
-min_mapscore=10
-# minimum identity between long read and reference (%)
-minID=60
-
+########### SCAFFOLDING PARAMETERS: ###########
+# minimum numbers of edges:
+min_edge=5.0
+num_sigma=`awk "BEGIN {print ($min_edge)/2.5}"`
 
